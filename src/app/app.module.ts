6bb3351804ebd9321458b623f2b12fb27fd57198
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-09-05 02:02:02 +0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { GithubSearchModule } from './github-search/github-search.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { GithubSearchModule } from './github-search/github-search.module';
     BrowserModule,
 
     GithubSearchModule,
+    StoreModule.provideStore({ counter: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
