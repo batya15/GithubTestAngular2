5b96c9140e62cb9a2934c5cdfbf030e06ac4fd75
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-09-05 21:44:38 +0300

    (CardComponent) feat: add tests

--- src/app/github-search/card-list/card/card.component.spec.ts
+++ src/app/github-search/card-list/card/card.component.spec.ts
@@ -1,10 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { FAKE_USER1 } from '../../test/fake-users';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
+  let de: DebugElement;
+  let eAvatar: HTMLElement;
+  let eTitle: HTMLElement;
+  let eLink: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +23,28 @@ describe('CardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.user = FAKE_USER1;
     fixture.detectChanges();
+    de = fixture.debugElement;
+    eAvatar = de.query(By.css('img')).nativeElement;
+    eTitle = de.query(By.css('h2')).nativeElement;
+    eLink = de.query(By.css('a')).nativeElement;
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have correct img', () => {
+    expect(eAvatar.getAttribute('src')).toContain(component.user.avatar);
+  });
+
+  it('should have correct title', () => {
+    expect(eTitle.textContent).toContain(component.user.login);
+  });
+
+  it('should have correct link', () => {
+    expect(eLink.getAttribute('href')).toContain(component.user.url);
+    expect(eLink.textContent).toContain(component.user.url);
   });
 });
